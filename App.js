1defc6111250088783605413da4180f6a1366667
Ryan Blaisdell <blaisdell.ryan11@gmail.com>
2024-02-28 19:35:46 -0500

    addded red 'Log Out' button that logs users out

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, StatusBar, ScrollView } from 'react-native';
-import Header from './src/navigation/Header';
+import { StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native';
 import Test from './src/components/Test';
 import HomeScreen from './src/screens/homePage';
 import RacePage from './src/screens/RacePage';
@@ -8,11 +7,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthProvider } from './src/components/AuthContext';
 import LoginScreen from './src/screens/LoginScreen';
-import { SignOutScreen} from './src/screens/index';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
-
-
+import { DrawerItem, createDrawerNavigator } from '@react-navigation/drawer';
+import { DrawerItemList } from '@react-navigation/drawer';
+import { useNavigation } from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -21,7 +18,7 @@ function MyStackNavigator() {
   return (
     <AuthProvider>
       <Stack.Navigator initialRouteName='Login'  screenOptions={{headerShown: false}}>
-        <Stack.Screen name='Login' component={LoginScreen}/>
+        <Stack.Screen name='Login' component={LoginScreen} options={{ headerShown: false, headerBackVisible: false }} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false, headerBackVisible: false }} />
         <Stack.Screen name="Test" component={Test} />
         <Stack.Screen style={styles.paddingPage} name="RacePage" component={RacePage} />
@@ -33,36 +30,35 @@ function MyStackNavigator() {
 export default function App() {
   const [showHeader, setShowHeader] = useState(true);
   return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <Drawer.Navigator
-          initialRouteName="Home"
-          drawerContent={(props) => <CustomDrawerContent {...props} />}
-        >
-          <Drawer.Screen name="Main" component={MyStackNavigator} options={{ title: 'Home' }} />
-          <Drawer.Screen name="SignOut" component={SignOutScreen} />
-          
-        </Drawer.Navigator>
-
-      </NavigationContainer>
-    </SafeAreaView>
+    <AuthProvider>
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home" drawerContent={(props) => <CustomDrawerContent {...props} />}>
+            <Drawer.Screen name="Main" component={MyStackNavigator} options={{ title: 'Home' }} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </AuthProvider>
   );
 
 }
 const CustomDrawerContent = (props) => {
+
+// IMPORTANT:  add a way to clear the email and password fields when returning to the login screen
+
+const navigation = useNavigation();
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView style={{ backgroundColor: '#ef4f9d' }}> 
-        <DrawerItemList {...props} />
+        <DrawerItemList {...props}/>
+        <DrawerItem
+          label="Sign Out"    // custom DrawerItem that gets added into the Drawer Navigator ; this way allows for custom colors and onPress functions
+          onPress={() => { navigation.navigate('Login') }}   // when pressed, it moves users to the Login Screen
+          style={{ backgroundColor: 'red' }} // Customize the background color
+          labelStyle={{ color: 'white' }}    // Customize the label color
+        />
       </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const Error = () => {
-  return (
-    <SafeAreaView style={styles.content}>
-      <Text> You shouldn't be seeing this page </Text>
     </SafeAreaView>
   );
 };
--- src/screens/SignOutScreen.js
+++ src/screens/SignOutScreen.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import { styles } from '../styles/GlobalStyles';
 
 const SignOutScreen = ({ navigation }) => {
     // Implement sign-out logic here    
 
+
+
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Sign Out Screen</Text>
-            <Button title="Sign Out" />
+            <Pressable>
+                <Text>Sign Out</Text>
+            </Pressable>
         </View>
     );
 };
