6cb65312543a7cafcf082183323f1697aa92258a
Sushanth Ambati <ambati23@students.rowan.edu>
2024-03-24 00:49:28 -0400

    added getUploadCredentials and uploadImage functions

--- src/components/imagePicker.js
+++ src/components/imagePicker.js
@@ -7,8 +7,8 @@ const processImage = async (uri) => {
     try {
         const manipResult = await ImageManipulator.manipulateAsync(
             uri,
-            [], // No specific operations like cropping or rotation
-            { compress: 0.8, format: ImageManipulator.SaveFormat.PNG }
+            [{resize: {width:1000}}], // No specific operations like cropping or rotation
+            { compress: 0.6, format: ImageManipulator.SaveFormat.PNG }
         );
         return manipResult.uri;
     } catch (error) {
@@ -59,6 +59,77 @@ export const handleSelectImage = async (uri) => {
         console.error("Error in handleSelectImage:", error);
         Alert.alert("Error cropping image");
         return uri; // Optionally return the original URI in case of error
+    }
+};
+
+export const getUploadCredentials = async (API_KEY, API_SECRET, race_id, race_event_days_id, album_id) => {
+    const apiURL = 'https://test3.runsignup.com/Rest/v2/photos/get-race-photo-upload-credentials.json';
+    //const API_KEY = "5LQODl7dxN1nyDqwTTra3rlbgslaV83a";
+    //const API_SECRET = "PnCFhbfsaQlBmTWlOenhlL0D1cvciyBB";
+    try {
+        const response = await fetch(`${apiURL}?race_id=${race_id}&race_event_days_id=${race_event_days_id}&rsu_api_key=${API_KEY}&album_id=${album_id}&`, {
+            method: 'GET',
+            headers: {
+                'x-rsu-api-secret': API_SECRET,
+            },
+        });
+        const json = await response.json();
+        
+        return json;
+
+    } catch (error) {
+        console.error("Error in getUploadCredentials:", error);
+        Alert.alert("Error getting upload credentials");
+    }
+};
+
+export const uploadImage = async (imageUri, credentials) => {
+    if (!credentials) {
+        console.log("Error getting upload credentials");
+        return;
+    }
+    const { action, method, metaKeys, acl, key, 'X-Amz-Credential': xAmzCredential, 'X-Amz-Algorithm': xAmzAlgorithm, 'X-Amz-Date': xAmzDate, Policy, 'X-Amz-Signature': xAmzSignature } = credentials;
+    const fileType = 'image/png'; 
+    const fileExtension = '.png'; 
+    const fileName = `${credentials.filePrefix}${new Date().getTime()}${fileExtension}`;
+    console.log("fileName is:", fileName);
+    console.log("action is:", action);
+    let formData = new FormData();
+    Object.keys(metaKeys).forEach(key => {
+        console.log("Key:", key, "Value:", metaKeys[key]);
+        formData.append(key, metaKeys[key]);
+    });
+    formData.append('acl', acl);
+    formData.append('key', key);
+    //formData.append('key', key.replace('${filename}', fileName));
+    formData.append('x-amz-meta-filename', key.replace('${filename}', fileName));
+    formData.append('X-Amz-Credential', xAmzCredential);
+    formData.append('X-Amz-Algorithm', xAmzAlgorithm); 
+    formData.append('X-Amz-Date', xAmzDate);
+    formData.append('Policy', Policy);
+    formData.append('X-Amz-Signature', xAmzSignature);
+    formData.append('file', { uri: imageUri, type: fileType, name: fileName });
+
+
+    for (const key in formData) {
+        console.log("Key:", key, "Value:", formData[key]);
+    }
+
+    try {
+        const uploadResponse = await fetch(action, {
+            method: method,
+            body: formData,
+        });
+
+        if (uploadResponse.ok) {
+            console.log("Image uploaded successfully");
+        } else {
+            errorBody = await uploadResponse.text();
+            console.log("Error uploading image", uploadResponse.status, errorBody);
+        }
+    } catch (error) {
+        console.error("Error in uploadImage:", error);
+        Alert.alert("Error uploading image");
     }
 };
 
