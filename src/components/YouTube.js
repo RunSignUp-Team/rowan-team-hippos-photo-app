8639b0f357988ce57e6e94ee1ba5a8a3120b1c3e
Sushanth Ambati <ambati23@students.rowan.edu>
2024-03-26 11:44:35 -0400

    added google authentication in Youtube

---
+++ src/components/YouTube.js
@@ -0,0 +1,17 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from 'react';
+
+
+const useGoogleAuthentication = () => {
+    const [userInfo, setUserInfo] = React.useState(null);
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        iosClientId: '412630371475-ssp3pb0atqjuec5spc6h1rkdia2e5a6p.apps.googleusercontent.com',
+        webClientId: '412630371475-e732mpa5cifq07qto2vh233uhdkd8ike.apps.googleusercontent.com'
+    });
+
+    return { userInfo, promptGoogleSignIn: promptAsync };
+};
+
+export default useGoogleAuthentication;
