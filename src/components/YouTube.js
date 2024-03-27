9dcd3ad3977210e7197b4f00bd3c0fdf8954db76
Sushanth Ambati <ambati23@students.rowan.edu>
2024-03-27 11:58:26 -0400

    added androidClientId

--- src/components/YouTube.js
+++ src/components/YouTube.js
@@ -8,7 +8,8 @@ const useGoogleAuthentication = () => {
     const [userInfo, setUserInfo] = React.useState(null);
     const [request, response, promptAsync] = Google.useAuthRequest({
         iosClientId: '412630371475-ssp3pb0atqjuec5spc6h1rkdia2e5a6p.apps.googleusercontent.com',
-        webClientId: '412630371475-e732mpa5cifq07qto2vh233uhdkd8ike.apps.googleusercontent.com'
+        webClientId: '412630371475-e732mpa5cifq07qto2vh233uhdkd8ike.apps.googleusercontent.com',
+        androidClientId: '412630371475-nlffu3mac6tgfhp0p1aa39eapmnhb1vk.apps.googleusercontent.com'
     });
 
     return { userInfo, promptGoogleSignIn: promptAsync };
